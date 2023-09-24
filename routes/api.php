bb04ba46b02560ede05e827bc0248f4b526b1ebc
Karen Rodriguez <karenrodriguez@artistshot.com>
2023-09-24 18:51:22 -0500

    Api login y seeder users

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\HomiliesController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,3 +23,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::apiResource("homilies", HomiliesController::class);
 Route::apiResource("users", UserController::class);
+Route::post('/login', [AuthController::class, 'login']);
